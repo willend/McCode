0366b4213c1362391d3a2145f6dc4bdfd9ecbbb4
Peter Willendrup <pkwi@fysik.dtu.dk>
2024-08-18 09:12:18 +0200

    Remove for now

--- tools/Python/mcdisplay/webgl/components/scene/info-view/import-json-button/ImportJSONButton.tsx
+++ tools/Python/mcdisplay/webgl/components/scene/info-view/import-json-button/ImportJSONButton.tsx
@@ -7,20 +7,6 @@ const ImportJSONButton = () => {
   const { instrument, setInstrument } = useInstrumentContext();
 
   const handleButtonClick = () => {
-    const comps = instrument.components;
-    let element = document.createElement("a");
-    element.setAttribute(
-      "href",
-      "data:text/plain;charset=utf-8," +
-        encodeURIComponent(JSON.stringify(comps, null, 2))
-    );
-    element.setAttribute("download", "components.json");
-    element.style.display = "none";
-    document.body.appendChild(element);
-
-    element.click();
-
-    document.body.removeChild(element);
   };
 
   return (
