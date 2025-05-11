55204e9becd4cc1b951296d4c6846604d095293c
Emmanuel FARHI <emmanuel.farhi.1@gmail.com>
2025-05-11 12:55:11 +0200

    cogen/py: on the way to unify .l/.y for both C and Python generators.

---
+++ mccode/src/pygen.h
@@ -0,0 +1,24 @@
+/*******************************************************************************
+*
+* McStas, neutron ray-tracing package
+*         Copyright (C) 1997-2009, All rights reserved
+*         Risoe National Laboratory, Roskilde, Denmark
+*         Institut Laue Langevin, Grenoble, France
+*
+* Kernel: port.h
+*
+* %Identification
+* Written by: K.N.
+* Date: Nov 11, 1998
+* Origin: Risoe
+* Release: McStas CVS_090602
+* Version: $Revision$
+*
+* Header file for portability related stuff.
+*
+* $Id$
+*
+*******************************************************************************/
+
+// activate Python generator (flex/bison/cogen)
+#define GENERATE_PY 1
