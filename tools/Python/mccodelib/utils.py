86bef0433b348cdc4a5238c1ba2aff2acf7fe3d8
Peter Willendrup <pkwi@fysik.dtu.dk>
2025-10-08 14:46:20 +0200

    Remove %INSTRUMENT_SITE and Modified by: entries from short description

--- tools/Python/mccodelib/utils.py
+++ tools/Python/mccodelib/utils.py
@@ -432,9 +432,9 @@ def parse_header(text):
       descrlines = []
       # remove all "*:" lines
       for l in m5.group(1).strip().splitlines():
-        # PW 2025: Suppressed the : detection,  parameters are looked for further down
-        # if not re.match(r'[^\n]*:', l, flags=re.DOTALL):
-        descrlines.append(l)
+        # PW 2025: Suppressed the : detection, but strip off %INSTRUMENT_SITE and Modified by 
+        if not re.match(r'%INSTRUMENT_SITE:', l, flags=re.DOTALL) and not re.match(r'Modified by:', l, flags=re.DOTALL):
+          descrlines.append(l)
       info.short_descr = '\n'.join(descrlines).strip()
     
     # description
