ad0b6b66e41cc5831de196c56766ba7a412066e3
Peter Willendrup <pkwi@fysik.dtu.dk>
2026-04-28 13:34:37 +0200

    More information from failures to logs

--- tools/Python/mctest/mctest.py
+++ tools/Python/mctest/mctest.py
@@ -394,6 +394,7 @@ def mccode_test(branchdir, testdir, limitinstrs=None, instrfilter=None, compfilt
             failed=True
             continue
 
+        resfile="(No file)"
         # test value extraction
         if not didwrite_nexus:
             extraction = extract_testvals(join(testdir, test.instrname, str(test.testnb)), test.detector)
@@ -414,17 +415,21 @@ def mccode_test(branchdir, testdir, limitinstrs=None, instrfilter=None, compfilt
                 test.testval=-1
                 failed=True
 
+        percent=0
         if test.didrun:
             if failed:
                 suffix += " + !! RUNTIME FAILURE - see % !! " % (resfile)
             formatstr = "%-" + "%ds: " % (maxnamelen+1) + \
                 "{:3d}.".format(math.floor(test.runtime)) + str(test.runtime-int(test.runtime)).split('.')[1][:2]
             if test.targetval!=0: # Normal situation, non-zero target value
-                logging.info(formatstr % test.get_display_name() + "    [val: " + str(test.testval) + " / " + str(test.targetval) + " = " + str(round(100.0*test.testval/test.targetval)) + " %]" + suffix)
+                percent=round(100.0*test.testval/test.targetval)
+                if percent<80 or percent>120
+                    suffix += " <--- BIG DISCREPANCY??"
+                logging.info(formatstr % test.get_display_name() + "    [val: " + str(test.testval) + " / " + str(test.targetval) + " = " + str(percent) + " %]" + suffix)
             else:                 # Special case, expected test target value is 0
                 logging.info(formatstr % test.get_display_name() + "    [val: " + str(test.testval) + " vs " + str(test.targetval) + " (absolute vs 0) ]" + suffix)
         else:
-            logging.info(formatstr % test.get_display_name() + " !! [TEST INDICATES RUNTIME ERROR - see % ] !!" % (resfile))
+            logging.info(formatstr % test.get_display_name() + " !! [TEST INDICATES RUNTIME ERROR - see %  + suffix ] !!" % (resfile))
         suffix=""
         # save test result to disk
         test.testcomplete = True
