0f7a9dd9271e3665f0b954fe7ee2fd9c40448177
Peter Willendrup <pkwi@fysik.dtu.dk>
2026-09-10 20:32:10 +0200

    Merge 2nd and 3rd doc lines to one

--- tools/Python/mcdoc/mcdoc.py
+++ tools/Python/mcdoc/mcdoc.py
@@ -599,14 +599,12 @@ class OverviewDocWriter:
  | <A href="%LINK_FILECOLON_SHARE%">share</A> ]
 </P>
 <P ALIGN=CENTER>
-[ <a href="file://%MCCODE_DOCDIR%/mcstas-manual.pdf">User Manual</a>
-| <a href="file://%MCCODE_DOCDIR%/mcstas-components.pdf">Component Manual</a> ]
-| <a href="file://%MCCODE_LIBDIR%/">McCode lib dir</a> ]
-</P>
-<P ALIGN=CENTER>
 [ <a href="README.html">Tool docs</a>
 | <a href="Instrument/INSTRUMENT_Grammar.html">Instrument Grammar</a>
-| <a href="Component/COMPONENT_Grammar.html">Component Grammar</a> ]
+| <a href="Component/COMPONENT_Grammar.html">Component Grammar</a>
+| <a href="file://%MCCODE_DOCDIR%/mcstas-manual.pdf">User Manual</a>
+| <a href="file://%MCCODE_DOCDIR%/mcstas-components.pdf">Component Manual</a> ]
+| <a href="file://%MCCODE_LIBDIR%/">McCode lib dir</a> ]
 </P>
 
 <CENTER><H1>Components and Instruments from the Library for <i>McStas</i></H1></CENTER>
